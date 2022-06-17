4a95c73f0c762a1e67dfc240d50d3d8555866ef6
Chris Allen <christopher.allen@fsgi.com>
2022-06-17 07:46:52 -0500

    Set up pattern for highlighting current page in nav

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,18 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Menu({ onMenuClose }) {
+  const router = useRouter();
+
   return (
     <aside id="menu">
       <a href="#" className="bi bi-x-lg" onClick={onMenuClose}></a>
       <nav>
         <ul>
-          <li>
+          <li className={router.pathname == '/' ? 'active' : undefined}>
             <Link href="/">Home</Link>
           </li>
-          <li>
+          <li className={router.pathname == '/about' ? 'active' : undefined}>
             <a>About</a>
           </li>
           <li>
