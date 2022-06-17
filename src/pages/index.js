9f2e65ba79c71b34773ad4d2606ab947a01d602a
Chris Allen <christopher.allen@fsgi.com>
2022-06-17 07:35:35 -0500

    Get rid of extra component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,40 @@
-import Intro from '../components/Intro';
+import Header from '../components/Header';
+import { useIsMenuOpen } from '../components/hooks';
+import Menu from '../components/Menu';
+import Summary from '../components/Summary';
 
 export default function Home() {
+  const { setIsMenuOpen } = useIsMenuOpen();
+
+  function onMenuOpen(e) {
+    e.preventDefault();
+    setIsMenuOpen(true);
+  }
+
+  function onMenuClose(e) {
+    e.preventDefault();
+    setIsMenuOpen(false);
+  }
+
   return (
     <>
-      <Intro />
+      <Menu onMenuClose={onMenuClose} />
+      <section id="intro">
+        <div className="wrapper">
+          <Header onMenuOpen={onMenuOpen} />
+          <div className="container">
+            <div className="inner">
+              <h1>
+                <span className="overlay">Hi, I&apos;m Chris</span>
+              </h1>
+              <p>
+                <span className="overlay">I build software and stuff</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <Summary />
+      </section>
     </>
   );
 }
