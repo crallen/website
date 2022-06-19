63c3fe2ceacb6b51047b462a51aabc09092c3f1c
Chris Allen <christopher.allen@fsgi.com>
2022-06-19 16:39:53 -0500

    Revise about section

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -2,20 +2,22 @@ export default function Summary() {
   return (
     <div id="summary">
       <div className="container">
+        <h2>About Me</h2>
         <div className="about">
-          <h2>About Me</h2>
-          <p>
-            I have been building software for over 15 years and I love it. I specialize in system
-            design, backend development, and platform engineering. I have extensive experience
-            building cloud infrastructure and implementing DevOps practices.
-          </p>
-          <p>
-            When I&apos;m not working, I&apos;m spending time with my wife, son, and our dogs. We
-            like to travel when we get the chance and I do a lot of cooking, which I&apos;ve turned
-            into a hobby.
-          </p>
+          <div>
+            <p>
+              I have been building software for over 15 years and I love it. I specialize in system
+              design, backend development, and platform engineering. I have extensive experience
+              building cloud infrastructure and implementing DevOps practices.
+            </p>
+            <p>
+              When I&apos;m not working, I&apos;m spending time with my wife, son, and our dogs. We
+              like to travel when we get the chance and I do a lot of cooking, which I&apos;ve
+              turned into a hobby.
+            </p>
+          </div>
+          <div className="me"></div>
         </div>
-        <div className="me"></div>
       </div>
     </div>
   );
