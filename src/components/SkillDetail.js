56b2713e9df60ef0a65389fcf40f5f8ec1bb1ee1
Chris Allen <christopher.allen@fsgi.com>
2022-06-19 14:58:42 -0500

    Start skills section and tweak colors

---
+++ src/components/SkillDetail.js
@@ -0,0 +1,8 @@
+export default function SkillDetail({ title, children }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      {children}
+    </div>
+  );
+}
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,24 @@
+import SkillDetail from './SkillDetail';
+
 export default function Skills() {
   return (
     <section id="skills">
       <div className="container">
         <h2>What I Do</h2>
+        <div className="skill-list">
+          <SkillDetail title="System Design">
+            <p>Text here</p>
+          </SkillDetail>
+          <SkillDetail title="Backend Development">
+            <p>Text here</p>
+          </SkillDetail>
+          <SkillDetail title="DevOps Practices">
+            <p>Text here</p>
+          </SkillDetail>
+          <SkillDetail title="Cloud Infrastructure">
+            <p>Text here</p>
+          </SkillDetail>
+        </div>
       </div>
     </section>
   );
