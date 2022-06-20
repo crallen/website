a723f69652d36382531457c1f3f8d4931444f4a0
Chris Allen <christopher.allen@fsgi.com>
2022-06-20 11:07:46 -0500

    Finish skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,16 +7,43 @@ export default function Skills() {
         <h2>What I Do</h2>
         <div className="skill-list">
           <SkillDetail title="System Design">
-            <p>Text here</p>
+            <p>
+              I plan and build systems that leverage a variety of technologies. I have experience
+              working with monoliths, microservice architecture, and systems in various states in
+              between.
+            </p>
           </SkillDetail>
           <SkillDetail title="Backend Development">
-            <p>Text here</p>
+            <p>
+              I build applications and web services in many different languages, including Go, Java,
+              TypeScript/JavaScript, Python, Ruby, and C#.
+            </p>
+          </SkillDetail>
+          <SkillDetail title="Embedded Development">
+            <p>
+              I&apos;ve written embedded applications for IoT in C++ and Java. I&apos;ve been
+              experimenting with Rust for this as well.
+            </p>
+          </SkillDetail>
+          <SkillDetail title="Frontend Development">
+            <p>
+              While not something I specialize in these days, I know my way around frontend
+              development because it is how I got my start in the industry. I try to keep up with
+              what&apos;s going on in this world.
+            </p>
           </SkillDetail>
           <SkillDetail title="DevOps Practices">
-            <p>Text here</p>
+            <p>
+              Implementing practices such as CI/CD and Infrastructure as Code is second nature to
+              me. If there is a process that can be automated, I will automate it.
+            </p>
           </SkillDetail>
           <SkillDetail title="Cloud Infrastructure">
-            <p>Text here</p>
+            <p>
+              I&apos;ve worked with Amazon Web Services and Google Cloud Platform in depth. I work
+              with technologies such as Docker, Kubernetes, and cloud native applications on a
+              regular basis.
+            </p>
           </SkillDetail>
         </div>
       </div>
